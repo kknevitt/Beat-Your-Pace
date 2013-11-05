e1ce8ee88c1131d27aec431c4a73bebdc9c81607
Laura <giuffridait@gmail.com>
2013-11-05 16:15:44 +0000

    Added Activity classes for Training Mode and Settings, added related activites into the manifest. Changed the code within the MainActivity to run the start screen, from this you can navigate to the Training Mode (with music playback functions) and settings.

--- BeatYourPace/src/com/example/beatyourpace/MainActivity.java
+++ BeatYourPace/src/com/example/beatyourpace/MainActivity.java
@@ -1,15 +1,32 @@
 package com.example.beatyourpace;
 
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener{
+	
+    Button b1, b2, b3;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		//creating image buttons objects and getting their setup from xml
+        b1 = (Button) findViewById(R.id.bTrainingMode); //goes to training mode view
+        b2 = (Button) findViewById(R.id.bLetsRun); //goes to lets run view
+        b3 = (Button) findViewById(R.id.bSettings); //goes to settings view
+        
+        //setting an event listener for each button
+        b1.setOnClickListener(this);
+        b2.setOnClickListener(this);
+        b3.setOnClickListener(this);
 	}
 
 	@Override
@@ -18,5 +35,18 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	@Override
+	public void onClick(View v) {
+	            // TODO Auto-generated method stub
+	      if(v == b1){ //play function
+              Intent intent = new Intent(this,TrainingModeActivity.class);
+              startActivity(intent);
+				}
+	      if (v == b3) { // settings
+	    	  Intent intent = new Intent(this,SettingsActivity.class);
+	    	  startActivity(intent);
+	      }
+	}       
+	
 
 }
---
+++ BeatYourPace/src/com/example/beatyourpace/TrainingModeActivity.java
@@ -0,0 +1,84 @@
+package com.example.beatyourpace;
+	
+
+import android.app.Activity;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
+
+public class TrainingModeActivity extends Activity implements OnClickListener {
+
+	int[] tracks = new int[2];
+    int currentTrack = tracks[0];
+    private MediaPlayer mediaPlayer = null;
+    
+    ImageButton imagebutton1, imagebutton2, imagebutton4, imagebutton5;
+    
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.trainingmode); 
+	   
+		
+	    //setting tracks path
+		tracks[0] = R.raw.song;
+	    tracks[1] = R.raw.song1;
+		
+		//creating image buttons objects and getting their setup from xml
+        imagebutton1 = (ImageButton) findViewById(R.id.bPlaySong); //play
+        //imagebutton2 = (ImageButton) findViewById(R.id.imageButton2); //pause
+        imagebutton4 = (ImageButton) findViewById(R.id.bSkipTrack); //next
+        imagebutton5 = (ImageButton) findViewById(R.id.bPreviousTrack); //last
+        
+        //setting an event listener for each button
+        imagebutton1.setOnClickListener(this);
+       // imagebutton2.setOnClickListener(this);
+        imagebutton4.setOnClickListener(this);
+        imagebutton5.setOnClickListener(this);
+    
+   }
+
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	@Override
+	public void onClick(View v) {
+	            // TODO Auto-generated method stub
+	      if(v == imagebutton1){ //play function
+	            	mediaPlayer = MediaPlayer.create(getApplicationContext(), tracks[currentTrack]);
+				    mediaPlayer.start();
+				}
+	           // if(v == imagebutton2){ //pause function
+	           // 	if (mediaPlayer.isPlaying()){
+	    	//			mediaPlayer.pause();
+	        //    		}            
+	        //    }
+	            if(v == imagebutton4){ //skip next song function
+	            	 mediaPlayer.release();
+	                 if (currentTrack < tracks.length) {
+	                   currentTrack++;
+	                   mediaPlayer = MediaPlayer.create(getApplicationContext(), tracks[currentTrack]);
+	                   mediaPlayer.start();
+	            } 
+	            if(v == imagebutton5){ //back function
+	                 mediaPlayer.release();
+	                 if (currentTrack < tracks.length) {
+	                	 if(currentTrack == 0){
+	                		 currentTrack = tracks.length -1;
+	                		 mediaPlayer = MediaPlayer.create(getApplicationContext(), tracks[currentTrack]);
+	                		 mediaPlayer.start();	        
+	                   }		
+	                	 }
+	            	}
+	            }
+	}
+
+}
