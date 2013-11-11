b95bc286f1087aa69c5065af421ff1b3688a735e
kknevitt <knev123@hotmail.com>
2013-11-11 15:46:53 +0000

    Changed Music functionality to be objects instead, though still needs work

--- BeatYourPace/src/com/GC01/BeatYourPace/MusicPlayer/MusicPlayer.java
+++ BeatYourPace/src/com/GC01/BeatYourPace/MusicPlayer/MusicPlayer.java
@@ -1,5 +1,7 @@
 package com.GC01.BeatYourPace.MusicPlayer;
 
+import java.io.IOException;
+
 import android.media.MediaPlayer;
 import android.app.Activity;
 import android.content.Context;
@@ -10,28 +12,41 @@ import android.os.Bundle;
 
 
 
-public class MusicPlayer {
+public class MusicPlayer {	 
 	
 	//MusicRetriever List = new MusicRetriever(); // do we need an object from Music Retriever
 	
-	private static final MediaPlayer mediaPlayer = new MediaPlayer();
-	static Context context;
-	static MusicTrackList tracklist = new MusicTrackList();
+	public MusicPlayer(){
 	
-	public static void play() {
-		mediaPlayer.setDataSource(MusicTrackList.currentTrack);
-		  mediaPlayer.start();		
+
+	
 	}
 	
-	public static void skip() {
-		MusicTrackList.setCurrentTrack(2);
-		  MediaPlayer.create(context, tracklist.getCurrentTrack());
+
+	
+	public void play() throws IllegalArgumentException, SecurityException, IllegalStateException, IOException {
+		Context context;
+		MusicTrackList tracklist = new MusicTrackList();
+		MediaPlayer mediaPlayer = new MediaPlayer();
+		mediaPlayer.setDataSource(tracklist.getCurrentTrack());
+		mediaPlayer.start();		
+	}
+	
+	public void skip() throws IllegalArgumentException, SecurityException, IllegalStateException, IOException {
+		Context context;
+		MusicTrackList tracklist = new MusicTrackList();
+		MediaPlayer mediaPlayer = new MediaPlayer();
+		tracklist.setCurrentTrack(2);
+		  mediaPlayer.setDataSource(tracklist.getCurrentTrack());
 		  mediaPlayer.start();	
 	}
 	
-	public static void previous() {
-		MusicTrackList.setCurrentTrack(3);
-		  MediaPlayer.create(context, tracklist.getCurrentTrack());
+	public void previous() throws IllegalArgumentException, SecurityException, IllegalStateException, IOException {
+		Context context;
+		MusicTrackList tracklist = new MusicTrackList();
+		MediaPlayer mediaPlayer = new MediaPlayer();
+		tracklist.setCurrentTrack(3);
+		  mediaPlayer.setDataSource(tracklist.getCurrentTrack());
 		  mediaPlayer.start();	
 	}
 
--- BeatYourPace/src/com/GC01/BeatYourPace/MusicPlayer/MusicTrackList.java
+++ BeatYourPace/src/com/GC01/BeatYourPace/MusicPlayer/MusicTrackList.java
@@ -1,32 +1,40 @@
 package com.GC01.BeatYourPace.MusicPlayer;
 
 public class MusicTrackList {
-
-static int currentTrack = 0;
+	
+	public String currentTrack;
+	
+	
+	public MusicTrackList(){
+		
+	}
 
 	
 	// Method for changing the current track depending on which button is pressed from the UI.
-	@SuppressWarnings("static-access")
-	public static void setCurrentTrack(int function) {
+	
+	public void setCurrentTrack(int function) {
 		
-		MusicRetriever music = new MusicRetriever();
-		currentTrack = music.tracks[0]; // Default value for current track
+		MusicRetriever music = new MusicRetriever(); //(ArrayListfromDB);
+		currentTrack = music.getSongPath(); // Default value for current track
 		
 		if (function == 1) // Play
-			currentTrack = music.tracks[0];
-		if (function == 2) // Skip
-			currentTrack++;
-		if (function == 3) // Previous
-			currentTrack--;	
+			currentTrack = music.getSongPath();
+		
+		
+		else if (function == 2) { // Skip
+			music.setSong();
+			currentTrack = music.getSongPath();
+		}
+		
+		else if (function == 3); {
+			// Previous	
+		}
 	}
+
 	
 	
-	public static int getCurrentTrack() {
+	public String getCurrentTrack() {
 		return currentTrack;
 		
 	}
-	
-	
-	
-	
 }
