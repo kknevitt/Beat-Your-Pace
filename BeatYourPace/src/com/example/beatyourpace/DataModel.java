d293fd7c179774fa35937902ee77e37351e2b181
shiress <sarahnicholson@dhcp-32-161.xlate.eduroam.ucl.ac.uk>
2013-11-11 12:25:40 +0000

    Database
    Synching to get KK updates, note that the database files are not
    complete!

--- BeatYourPace/src/com/example/beatyourpace/DataModel.java
+++ BeatYourPace/src/com/example/beatyourpace/DataModel.java
@@ -6,15 +6,39 @@ public class DataModel {
 	    /**
 	     * DEFINE THE TABLE
 	     */
-	    // ID COLUMN MUST LOOK LIKE THIS
-	    public static final String ID = "_id";
-	    public static final String BPM = "bpm";
-	    public static final String PACE = "pace";
-	    public static final String TITLE = "title";
-	    public static final String ARTIST = "artist";
+	    // Columns
+	    private static String ID = "_id";
+	    public static int BPM = 24;  //replace with actual
+	    public static double PACE = 6.0;  //replace with actual
+	    public static String TITLE = "title";
+	    public static String ARTIST = "artist";
 	    /**
 	     * DEFINE THE CONTENT TYPE AND URI
 	     */
 	    // TO BE done
+	      
+	 public DataModel(){}
+	     
+	        public DataModel(String ID, int BPM, double PACE, String TITLE, String ARTIST) {
+	            super();
+	            this.setID(ID);
+	            this.BPM = BPM;
+	            this.PACE = PACE;
+	            this.TITLE = TITLE;
+	            this.ARTIST = ARTIST;
+	        }
+	     
+	        //getter
+	        @Override
+	        public String toString() {
+	            return "DataModel [ID=" + getID() + ", BPM=" + BPM + ", pace=" + PACE +", title=" +TITLE +", artist=" + ARTIST +" ]";
+	        }
 
-}
+			public static String getID() {
+				return ID;
+			}
+
+			public static void setID(String iD) {
+				ID = iD;
+			}
+	   }
