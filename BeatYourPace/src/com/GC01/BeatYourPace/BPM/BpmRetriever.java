b7914708c17cf135121713ce3af96b8c9e7a7b0e
Laura <giuffridait@gmail.com>
2013-11-12 14:41:06 +0000

    Adding comments to BPM retriever

--- BeatYourPace/src/com/GC01/BeatYourPace/BPM/BpmRetriever.java
+++ BeatYourPace/src/com/GC01/BeatYourPace/BPM/BpmRetriever.java
@@ -1,7 +1,7 @@
 package com.GC01.BeatYourPace.BPM;
 
 import java.io.File;
-
+import java.io.IOException;
 import com.echonest.api.v4.EchoNestAPI;
 import com.echonest.api.v4.EchoNestException;
 import com.echonest.api.v4.Track;
@@ -9,20 +9,34 @@ import com.echonest.api.v4.TrackAnalysis;
 
 
 public class BpmRetriever {
-
 	
-	private static final String API_KEY = ("LKMQIIIK24AL58W6D");
+	//adding EchoNest API developer key
+	private static final String API_KEY = ("LKMQIIIK24AL58W6D"); 
+	
+	//instance of library class
+	EchoNestAPI echoNest = new EchoNestAPI(API_KEY);
+	
+	
+	//creating get method to query EchoNest library
+	double getBPM() throws EchoNestException, IOException{
 
-	public static void main (String [] args)throws EchoNestException, Exception{
-
-    EchoNestAPI echoNest = new EchoNestAPI(API_KEY);
+	//open file and uploads track to get BPM analysed
+	//to be replaced by 'get music' method
     File file = new File("C:\\Users\\Solange\\Desktop\\eclipse java\\BPM\\res\\raw\\04.mp3");
     Track track = echoNest.uploadTrack(file, true);
+    
+    //analysing track to get BPM
     track.waitForAnalysis(30000);
     TrackAnalysis a = track.getAnalysis();
     
+    //retrieve the BPM
+    double tempo = a.getTempo(); 
     
+    //temporary function
     System.out.print("Tempo " + a.getTempo());
+    
+    return tempo;
+   
 
-	
+	}
 }
