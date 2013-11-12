b4290a27a1c2dc0859c0c73e7eec106f4c01b3e7
shiress <sarahnicholson@Sarahs-MacBook-Air.local>
2013-11-12 14:17:50 +0000

    Revert "Merge branch 'master' of https://github.com/kknevitt/Beat-Your-Pace"
    This reverts commit fe61282839de33a949bc9a872149a065655ca297, reversing
    changes made to 20d9d8de5fc1d8f3dee01ca20b47c4846fd5c3ef.

--- BeatYourPace/src/com/GC01/BeatYourPace/MusicPlayer/MusicTrackList.java
+++ BeatYourPace/src/com/GC01/BeatYourPace/MusicPlayer/MusicTrackList.java
@@ -1,42 +1,32 @@
 package com.GC01.BeatYourPace.MusicPlayer;
 
 public class MusicTrackList {
-	
-	String currentTrack;	
-	
-	
-	public MusicTrackList(){
-	
-		
-	}
+
+static int currentTrack = 0;
 
 	
 	// Method for changing the current track depending on which button is pressed from the UI.
-	
-	public void setCurrentTrack(int function) {
+	@SuppressWarnings("static-access")
+	public static void setCurrentTrack(int function) {
 		
-		MusicRetriever music = new MusicRetriever(null); //(ArrayListfromDB);
-		currentTrack = music.getSongPath(); // Default value for current track
+		MusicRetriever music = new MusicRetriever();
+		currentTrack = music.tracks[0]; // Default value for current track
 		
 		if (function == 1) // Play
-			currentTrack = music.getSongPath();
-		
-		
-		else if (function == 2) { // Skip
-			music.setSong();
-			currentTrack = music.getSongPath();
-		}
-		
-		else if (function == 3); {
-			// Previous	
-		}
+			currentTrack = music.tracks[0];
+		if (function == 2) // Skip
+			currentTrack++;
+		if (function == 3) // Previous
+			currentTrack--;	
 	}
-
 	
 	
-	public String getCurrentTrack() {
+	public static int getCurrentTrack() {
 		return currentTrack;
 		
 	}
-
+	
+	
+	
+	
 }
