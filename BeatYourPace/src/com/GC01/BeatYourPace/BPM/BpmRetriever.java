63202a4f28295478287df007a81d6c726fe7d2cd
Laura <giuffridait@gmail.com>
2013-11-14 13:34:28 +0000

    Trying to resolve local conflicts.

--- BeatYourPace/src/com/GC01/BeatYourPace/BPM/BpmRetriever.java
+++ BeatYourPace/src/com/GC01/BeatYourPace/BPM/BpmRetriever.java
@@ -9,17 +9,17 @@ import com.echonest.api.v4.TrackAnalysis;
 
 
 public class BpmRetriever {
-	
-	//adding EchoNest API developer key
-	private static final String API_KEY = ("LKMQIIIK24AL58W6D"); 
-	
-	//instance of library class
-	EchoNestAPI echoNest = new EchoNestAPI(API_KEY);
-	
+		
 	
 	//creating get method to query EchoNest library
 	double getBPM() throws EchoNestException, IOException{
 
+	//adding EchoNest API developer key
+	final String API_KEY = ("LKMQIIIK24AL58W6D"); 
+		
+	//instance of library class
+	EchoNestAPI echoNest = new EchoNestAPI(API_KEY);
+		
 	//open file and uploads track to get BPM analysed
 	//to be replaced by 'get music' method
     File file = new File("C:\\Users\\Solange\\Desktop\\eclipse java\\BPM\\res\\raw\\04.mp3");
