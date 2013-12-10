2492767d76dede0e9b4f56587bd1b5a9815d4b77
Laura <giuffridait@gmail.com>
2013-12-10 16:34:37 +0000

    Making small changes to push.

--- BeatYourPace/src/com/GC01/BeatYourPace/HelpPage/HelpPageActivity.java
+++ BeatYourPace/src/com/GC01/BeatYourPace/HelpPage/HelpPageActivity.java
@@ -1,13 +1,9 @@
 package com.GC01.BeatYourPace.HelpPage;
 
 import com.example.beatyourpace.R;
-import com.example.beatyourpace.R.layout;
-import com.example.beatyourpace.R.menu;
 import com.google.analytics.tracking.android.EasyTracker;
-
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
 
 public class HelpPageActivity extends Activity {
 
