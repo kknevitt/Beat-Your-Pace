64bb87f22faee8a2245aed18dd6e91580ebe6f96
Laura <giuffridait@gmail.com>
2013-12-13 11:18:50 +0000

    Changes to the CurrentPace.

--- BeatYourPace/src/com/GC01/BeatYourPace/PaceCalculator/CurrentPace.java
+++ BeatYourPace/src/com/GC01/BeatYourPace/PaceCalculator/CurrentPace.java
@@ -1,6 +1,7 @@
 package com.GC01.BeatYourPace.PaceCalculator;
 
 import com.GC01.BeatYourPace.Main.ContextProvider;
+
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -42,7 +43,7 @@ public class CurrentPace extends Service {
     public void onCreate()
     {
       super.onCreate();  
-   // Acquire a reference to the system Location Manager
+      // Acquire a reference to the system Location Manager
       locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
       startService(context);
     }
@@ -51,61 +52,73 @@ public class CurrentPace extends Service {
 	 * provided by the Android API. The method takes into account the user speed unit preferences (defined on the Settings classes: 
 	 * miles per minute or kilometres per hour. **/
 	public void startService(Context context){
-	    Log.d(null, "testing - startService() for GPS is being called");
-		 LocationListener locationListener = new LocationListener(){		        
+	
+		Log.d(null, "testing - startService() for GPS is being called");
+		
+		LocationListener locationListener = new LocationListener(){		        
 			public void onLocationChanged(Location location){
 				
-				//Get the user preference for miles(1) or kilometres(2)
-				SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(ContextProvider.getContext());
-				int unitType = Integer.parseInt(preferences.getString("unitType", "1"));
-
 				// Called when a new location is found by the network location provider.
-				 if (location !=null) {    	
-					 if (unitType == 1) {
-			    		currentPace = (float) (location.getSpeed() / MPS_TO_MINS_PER_MILE);
-			    		sendGPSInfo();
+				 if (location !=null) {    		
+			    		currentPace = location.getSpeed();
+			    		addSpeedUnit(currentPace);
 			    		
-			    	    Log.d(null,"testingGPS M/M");
-			    		System.out.println(getGPSInfo());
+			    		//temporary toast message to verify if the GPS is working
+			    		Toast.makeText(getApplicationContext(), "The GPS is working",
+			 				   Toast.LENGTH_LONG).show();
 			    		
-			    			if (unitType == 2) {
-			    				currentPace = (float) (location.getSpeed() / MPS_TO_PER_KILOMETRES);
-			    				
-			    				
-			    				Log.d(null,"testing KM/M");
-			    				System.out.println(getGPSInfo());
-			    				}
-			    			}  
-				 }
-				 else if( location == null ) {
+			    	 } else if(location == null){
 						 currentPace = 0;
 							Toast.makeText(getApplicationContext(), "Your GPS is not responding, it may be off.",
-									   Toast.LENGTH_LONG).show();	
-				   } 				   	
-		      }
-			 
-			 /**Unimplemented methods**/
-		        public void onStatusChanged(String provider, int status, Bundle extras) {}
-		        public void onProviderEnabled(String provider) {}
-		        public void onProviderDisabled(String provider) {}
-		    };
-		    
-		    /**Setting the location provider and the updates interval time **/
-		    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, locationListener);
-		} 
+									   Toast.LENGTH_LONG).show();
+					 }
+				 }
+			/**Unimplemented methods**/
+	        public void onStatusChanged(String provider, int status, Bundle extras) {}
+	        public void onProviderEnabled(String provider) {}
+	        public void onProviderDisabled(String provider) {}
+	    };
+	    
+	    /**Setting the location provider and the updates interval time **/
+	    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, locationListener);
+	} 
+
+	/** Method created to adjust the currentPace retrieve to the preferred speed unit of the user.
+	 * The getSpeed() method retrieves speed at metres per second.*/
+	private void addSpeedUnit(float currentPace){
+		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(ContextProvider.getContext());
+		int unitType = Integer.parseInt(preferences.getString("unitType", "1"));
+		
+		if (unitType == 1){
+			currentPace = (float) (currentPace / MPS_TO_MINS_PER_MILE);
+			sendGPSInfo();
+			
+		}
+			if (unitType == 2) {
+			currentPace = (float) (currentPace / MPS_TO_PER_KILOMETRES);
+			sendGPSInfo();
+		}
+			
+	}	
+			    				
+			
 		
 	@Override
 	public IBinder onBind(Intent intent) {
 		return null;
 	}
 	
-	
+	/**Get the GPS current pace value**/
 	private float getGPSInfo(){
 		return currentPace;
 	}
 	
-	/**Sets the broadcast message about GPS current pace**/
+	/**Sets the broadcast message for the GPS current pace**/
 	private void sendGPSInfo() {
+		
+		//temporary toast message to test that the GPS data is being sent to the training mode class
+		Toast.makeText(getApplicationContext(), "send GPSInfo intent is being called.",
+				   Toast.LENGTH_LONG).show();
 		
 		  Intent intent = new Intent("Current Pace Event");
 		  
