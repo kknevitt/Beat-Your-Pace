e671020e1fe1b12a764f1af01f066d46651c8eb3
kknevitt <knev123@hotmail.com>
2014-01-21 16:54:55 +0000

    Fixing errors in song and current song

--- BeatYourPace/src/com/GC01/BeatYourPace/MusicPlayer/CurrentSong.java
+++ BeatYourPace/src/com/GC01/BeatYourPace/MusicPlayer/CurrentSong.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.GC01.BeatYourPace.MusicPlayer;
 
 import com.GC01.BeatYourPace.Database.DatabaseAdapter1;
@@ -54,47 +53,3 @@ import com.GC01.BeatYourPace.Main.ContextProvider;
 	}	
 
 }
-=======
-package com.GC01.BeatYourPace.MusicPlayer;
-
-/** 
- * @author Kristian Knevitt
- * @version 1.0, Updated 12/12/2013
- */
-
-public class CurrentSong extends Song {
-	
-	private static TrackList trackList = TrackList.getInstance();
-	
-	private static CurrentSong _instance = null;
-
-	
-	private CurrentSong() {
-		
-		super();
-		
-	}
-	
-	/** Method for creating a Singleton instance of the Current Song if one does not already exist */
-	public static CurrentSong getInstance() {
-		if(_instance == null)
-		{
-			_instance = new CurrentSong();
-		}
-		return _instance;
-	}
-
-	
-	/** Sends the file path  of the current song by using the track index from the TrackList.
-	 *
-	 * @return songPath (String)The file path for the current song.
-	 */
-	public String getSongPath() {
-				
-		songPath = trackList.getTrackIndex(trackList.getTrackIndex());
-		return songPath;
-				
-		}	
-
-}
->>>>>>> 8ff051f2ad689ef4643f6f096f75075d01e7b679
