62cfe0c81bd8e1decb182e0e6b18b14d726de08b
giuffridait <giuffridait@gmail.com>
2014-01-24 10:49:24 +0000

    Target Pace giving errors again

--- BeatYourPace/src/com/GC01/BeatYourPace/PaceCalculator/TargetPace.java
+++ BeatYourPace/src/com/GC01/BeatYourPace/PaceCalculator/TargetPace.java
@@ -2,11 +2,7 @@ package com.GC01.BeatYourPace.PaceCalculator;
 
 import com.GC01.BeatYourPace.Main.LetsRunModeActivity;
 import com.GC01.BeatYourPace.Main.TrainingModeActivity;
-<<<<<<< HEAD
 
-=======
-
->>>>>>> 7a0962926e6eb1e88cf1f4966c319561971e1c7c
 /**
  * Calculates the target pace that is changed when the user selects increase or decrease target pace from the run modes
  * 
@@ -48,19 +44,8 @@ public class TargetPace {
 		
 	}
 	
-<<<<<<< HEAD
-/** 
- *Retrieves the TargetPace from the training mode class if previously set. 
- */
-	
-=======
 
 
-/** 
- *Retrieves the TargetPace from the training mode class if previously set. 
- */
-
->>>>>>> 7a0962926e6eb1e88cf1f4966c319561971e1c7c
 
 	/**
 	 * Sets the new target pace
